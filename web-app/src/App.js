3e2f4381a327166f6ad9fc92749a1a3a2d718796
Amir Pacolli <amir.pacolli@poviolabs.comamir.pacolli@poviolabs.comamir.pacolli@poviolabs.comamir.pacolli@poviolabs.com>
2022-03-17 19:30:41 +0100

    Changed app title

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           First React App deployed to AWS EC2
+           First React App deployed to AWS EC2 - Version 2
         </p>
         <a
           className="App-link"
